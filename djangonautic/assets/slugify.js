661922c5f98c95db58d2e055274c3643e89b9144
Feranmi Ajala <ajalaferanmi123@gmail.com>
2022-12-30 14:25:10 +0100

    Initial commit

---
+++ djangonautic/assets/slugify.js
@@ -0,0 +1,15 @@
+const titleinput=document.querySelector('input[name = title');
+const sluginput=document.querySelector('input[name = slug');
+
+const slugify = (val)=>{
+    return val.toString().toLowerCase().trim()
+    .replace(/&/g,'-and-')   //replace and with hyphen and 
+    .replace(/[\s\W-]+/g, '-')  //replaces spaces, non word chars and dashes with a single dash
+}; 
+
+
+titleinput.addEventListener('keyup', (e)=>
+{
+    sluginput.setAttribute('value', slugify(titleinput.value));
+
+});
